f7503bc88a47105d0554e4eb54193ff7cfd0011f
Joris Berthelot <joris.berthelot@careerbuilder.com>
2021-01-05 03:01:53 +0100

    Linting...

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"math/rand"
 	"os"
 	"time"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/eexit/http2smtp/internal/api"
@@ -17,9 +15,7 @@ import (
 	"github.com/rs/zerolog"
 )
 
-var adapter *httpadapter.HandlerAdapter
-
-func init() {
+func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	var e env.Bag
@@ -45,14 +41,7 @@ func init() {
 	)
 
 	app := api.New(e, logger, smtpClient, converterProvider)
-	adapter = httpadapter.New(app.Wrap(app.Mux()))
-}
+	adapter := httpadapter.New(app.Wrap(app.Mux()))
 
-// Handler handles the function invocation
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return adapter.ProxyWithContext(ctx, req)
-}
-
-func main() {
-	lambda.Start(Handler)
+	lambda.StartHandler(lambda.NewHandler(adapter.ProxyWithContext))
 }
